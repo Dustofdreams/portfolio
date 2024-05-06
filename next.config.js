c1ceef2efc66df01509a91a43324d0dcd0db2e37
DustOfDreams <52716391+Dustofdreams@users.noreply.github.com>
2024-05-06 17:36:19 +0300

    changed nextConfit basePath

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /**
  * @type {import('next').NextConfig}
  */
+
+const isProd = process.env.NODE_ENV === "developlmet";
+
 const nextConfig = {
+    basePath: isProd? '/portfolio' : '',
     output: 'export',
     distDir: 'dist',
   }
